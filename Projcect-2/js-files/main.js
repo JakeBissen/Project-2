5605deeff8e616561d57ac54e8c0b8fbf28d6162
JakeBissen <Joy303jake303@gmail.com>
2025-09-30 14:12:05 -0500

    gif display box working and set, moving onto colors

--- Projcect-2/js-files/main.js
+++ Projcect-2/js-files/main.js
@@ -1,20 +1,4 @@
-/* 
-<h1 id="title" class="title">THE GIF GENERATOR</h1>
-<button id="jiff" onclick="turnJiff()">Click for Jiff</button>
 
-<div id="js-image-appear-here"></div>
-<br>
-
-<input id="search" type="text" placeholder="Search for a gif">
-
-<br>
-
-<button id="btnSearch" onclick="generateImg()">Click to Generate Gif</button>
-
-    
-
-<p>Placeholder text</p>
-*/
 const title = document.getElementById('title');
 const search = document.getElementById('search');
 const btnSearch = document.getElementById('btnSearch');
