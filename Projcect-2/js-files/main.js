383b284f37e284063ae6f715dd0132b242242b32
JakeBissen <Joy303jake303@gmail.com>
2025-10-02 13:06:25 -0500

    added css finshes to index

--- Projcect-2/js-files/main.js
+++ Projcect-2/js-files/main.js
@@ -35,6 +35,7 @@ function turnJiff() {
 
 let imageHere = document.getElementById('js-image-appear-here');
 
+
 async function generateImg() {
   const query = search.value.trim(); 
   if (!query) {
@@ -46,27 +47,43 @@ async function generateImg() {
   }
 
   const apiKey = "PmclBFOvDxc7GOSn7wQDXdYm20OSjF99";
-  const url = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=${encodeURIComponent(query)}&rating=r`;
-
+  const url =  `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${encodeURIComponent(query)}&limit=50&offset=0&rating=r`;
   try {
     const response = await fetch(url);
     const result = await response.json();
 
-     imageHere.innerHTML = "";
+     imageHere.innerHTML = " ";
+  
+    if (result.data && result.data.length > 0) {
+  const shuffled = result.data.sort(() => 0.5 - Math.random());
+  const selected = shuffled.slice(0, 10);
 
 
-    if (result.data) {
-      const gifUrl = result.data.images.original.url;
-      const img = document.createElement("img");
-      img.src = gifUrl;
-      img.alt = query;
-            imageHere.appendChild(img);
-    } else {
-      imageHere.textContent = "No results found, please check your spelling or enter a different search term."
-      imageHere.style.fontFamily = "Bangers, system-ui";
-    imageHere.style.fontWeight = "400";
-    imageHere.style.fontStyle = "normal";;
-    }
+  for (let gif of selected) {
+  
+  const gifUrl = gif.images.original.url;
+
+  const gifBox = document.createElement("div");
+  gifBox.classList.add("gif-box");
+
+  const img = document.createElement("img");
+  img.src = gifUrl;
+  img.alt = query;
+
+
+
+  gifBox.appendChild(img);
+  imageHere.appendChild(gifBox);
+}
+
+
+} else {
+  imageHere.textContent = "No results found, please check your spelling or enter a different search term.";
+  imageHere.style.fontFamily = "Bangers, system-ui";
+  imageHere.style.fontWeight = "400";
+  imageHere.style.fontStyle = "normal";
+}
+
   } catch (err) {
     console.error(err);
     imageHere.textContent = "No results found, please check your spelling or enter a different search term.";
